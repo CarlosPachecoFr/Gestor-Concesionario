2d80e3e7d10c087b0f6ad1925301acb2c82af9e2
CarlosPachecoFr <carlospf1717@gmail.com>
2025-10-25 13:21:51 +0200

    Creado componente para mostrar inventario.

---
+++ src/app/componentes/home/mostrar-inventario/mostrar-inventario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrar-inventario',
+  imports: [],
+  templateUrl: './mostrar-inventario.component.html',
+  styleUrl: './mostrar-inventario.component.css'
+})
+export class MostrarInventarioComponent {
+
+}
